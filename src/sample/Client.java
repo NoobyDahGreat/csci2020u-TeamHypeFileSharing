5fe0ac72c85f06947ebc0a80c13ba2f9a9fc160a
juda96 <itsjude96@gmail.com>
2016-03-29 20:05:08 -0400

    Added function call for button actions. Changed taking cmd args to taking user input through GUI

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -12,9 +12,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -23,6 +25,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Observable;
+import java.util.Optional;
 
 public class Client extends Application {
     private static String[] cmdArgs;
@@ -34,57 +37,26 @@ public class Client extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        File resource = new File("./resources");
-        if (!resource.exists()) {
-          resource.mkdir();
-        }
-        //uncomment when ready for final product
-        /*
-        if (cmdArgs.length == 0) {
-            System.exit(0);
-        }*/
+        // Get hostname from user
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("File Sharer v1.0");
+        dialog.setHeaderText("Setup Phase");
+        dialog.setContentText("Please enter the name of your computer:");
 
-        String computerName;
-        String path;
-        /*
-        if (cmdArgs.length > 0) {
-            computerName = cmdArgs[0];
-            path = cmdArgs[1];
-        }*/
-        Socket socket;
-        BufferedReader in;
-        PrintWriter out;
-
-        try {
-            // connect to the server (3-way connection establishment handshake)
-            socket = new Socket("127.0.0.1", 8080);
-
-            // wrap the input streams into readers and writers
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream());
-
-            // send the HTTP request GET /yahoo/yahoo.html HTTP/1.0\n\n
-            String request = "DIR";
-            String delim = "\r\n";
-            out.print(request + delim );
-            out.flush();
-
-            // read and print the response
-            String response;
-            System.out.println("Response:");
-            while ((response = in.readLine()) != null) {
-                System.out.println(response);
-            }
-
-            // close the connection (3-way tear down handshake)
-            out.close();
-            in.close();
-            socket.close();
-        } catch (Exception e) {
-            System.out.println(e);
-            e.printStackTrace();
+       // Traditional way to get the response value.
+        String computerName = " ";
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            computerName = result.get();
         }
 
+        // Dialog promting user to choose dir
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        File sharedDirectory = directoryChooser.showDialog(primaryStage);
+        String sharedDir = sharedDirectory.getAbsolutePath();
+
+        RequestSender requestSender = new RequestSender(computerName, 8081, sharedDir);
 
         summary = new GridPane();
 
@@ -94,17 +66,16 @@ public class Client extends Application {
         summary.setHgap(3);
 
         clientList = new ListView();
-        ObservableList<String> items = FXCollections.observableArrayList("Sup", "Nerd");
-//        File dir = new File("args[1]");
-//        File[] list = dir.listFiles();
-//
-//        for(int i =0; i < list.length; i++){
-//            if(list[i].isFile()){
-//                if(list[i].getName().endsWith(".txt")){
-//                    items.add(list[i].getName());
-//                }
-//            }
-//        }
+        ObservableList<String> items = FXCollections.observableArrayList();
+        File[] list = sharedDirectory.listFiles();
+
+        for(int i =0; i < list.length; i++){
+            if(list[i].isFile()){
+                if(list[i].getName().endsWith(".txt")){
+                    items.add(list[i].getName());
+                }
+            }
+        }
 
         clientList.setItems(items);
         clientList.setPrefWidth(200);
@@ -114,7 +85,7 @@ public class Client extends Application {
         String item = clientList.getSelectionModel().getSelectedItem();
 
         serverList = new ListView();
-
+        ObservableList<String> serverItems = requestSender.dir();
 
         serverList.setItems(null);
         serverList.setPrefHeight(400);
@@ -125,7 +96,8 @@ public class Client extends Application {
         download.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                System.out.println(item);
+                requestSender.download(item);
             }
         });
         summary.add(download, 0, 0);
@@ -134,7 +106,7 @@ public class Client extends Application {
         upload.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                requestSender.upload("./shared_files/aaignn3_commands.txt");
             }
         });
         summary.add(upload, 1, 0);
@@ -159,7 +131,6 @@ public class Client extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
 
